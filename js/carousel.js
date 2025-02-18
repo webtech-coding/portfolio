0b543883dd37760500ba5a25c33a7a3d6c598b8d
shovit <webtech.delver@gmail.com>
2025-02-18 13:24:06 +0200

    portfolio initiated

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.skills__tools').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        centerMode: true,
+      
+    });
+  });
